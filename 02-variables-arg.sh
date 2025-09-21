274f8998bbf2adc1361ff2449068ffecfd949681
Rama Krishna <ramkrish2537@gmail.com>
2025-09-21 23:51:49 +0530

    variables-arg-push

--- 02-variables-arg.sh
+++ 02-variables-arg.sh
@@ -5,5 +5,5 @@ PERSON2=$2
 
 echo "$PERSON1":: hi "$PERSON2"
 echo "$PERSON2" :: hello "$PERSON1"
-echo "$PERSON1" :: How are you doing today
-echo "$PERSON2":: I'm doing good
+echo "$PERSON1:: How are you doing today"
+echo "$PERSON2:: I'm doing good"
