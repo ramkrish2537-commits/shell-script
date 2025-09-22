21dbdfee62fa6ebca09fc28a7b956935e6b7ccdf
Rama Krishna <ramkrish2537@gmail.com>
2025-09-22 06:10:42 +0530

    variables-datatypes

---
+++ 04-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FAMILY=("RAMA","LOV","ABHI","JAY")
+
+echo "Father of the house: ${FAMILY[0]}"
+echo "Mother of the house: ${FAMILY[1]}"
+echo "Elder son of the house: ${FAMILY[2]}"
+echo "Younger sonf the house: ${FAMILY[3]}"
+
+echo "All family members names: ${FAMILY[4]}"
