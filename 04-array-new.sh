9e3318acace88a3320938fad0bec70805f4f5731
Rama Krishna <ramkrish2537@gmail.com>
2025-09-22 06:20:30 +0530

    variables-datatypes

---
+++ 04-array-new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FAMILY=("RAMA" "LOV" "ABHI" "JAY")
+
+echo "Head of the family is: ${FAMILY[0]}"
+echo "Mother of the family is: ${FAMILY[1]}"
+echo "First son of the family is: ${FAMILY[2]}"
+echo "Second son of the family is: ${FAMILY[3]}"
+
+echo "All family member are: ${FAMILY[@]}
