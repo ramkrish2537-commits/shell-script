8e61a8a9bb8689ba828c21552bd17da130d76e08
Rama Krishna <ramkrish2537@gmail.com>
2025-09-22 06:20:58 +0530

    variables-datatypes

--- 04-array-new.sh
+++ 04-array-new.sh
@@ -7,4 +7,4 @@ echo "Mother of the family is: ${FAMILY[1]}"
 echo "First son of the family is: ${FAMILY[2]}"
 echo "Second son of the family is: ${FAMILY[3]}"
 
-echo "All family member are: ${FAMILY[@]}
+echo "All family member are: ${FAMILY[@]}"
